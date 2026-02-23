f6429286bb8c0e15f9b99634fb81a65ae729c22a
Timur <timur@atalay.io>
2026-02-23 15:18:59 -0800

    Refactor

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,12 @@ import globals from 'globals'
 import js from '@eslint/js'
 
 export default [
-  // Ignore build and dist directories
   { ignores: ['dist', 'examples', 'test'] },
-  // Use recommended ESLint configurations
   js.configs.recommended,
   {
     languageOptions: {
-      // Use the latest ECMAScript version
       ecmaVersion: 'latest',
-      // Define global variables
       globals: { numara: 'readonly', ...globals.browser, ...globals.node, luxon: 'readonly' },
-      // Set the source type to module
       sourceType: 'module'
     }
   }
