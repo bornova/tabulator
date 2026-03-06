4a65688670c26f2931d7291f7394d05e30002272
Timur <timur@atalay.io>
2026-03-06 15:55:45 -0800

    Refactor

---
+++ scripts/build-css.js
@@ -0,0 +1,23 @@
+import fs from 'fs-extra'
+import path from 'path'
+import { compile } from 'sass'
+import { globbySync } from 'globby'
+
+const inputFiles = globbySync(['src/scss/themes/*/{,*/}[!_]*.scss'])
+
+process.stdout.write('Compiling SCSS files...')
+
+for (const inputFile of inputFiles) {
+  const relativeFile = inputFile.replace('src/scss/', '')
+  const outputCss = `dist/css/${relativeFile.replace('.scss', '.css')}`
+  const outputMinCss = `dist/css/${relativeFile.replace('.scss', '.min.css')}`
+
+  const fullResult = compile(inputFile, { style: 'expanded' })
+  const minResult = compile(inputFile, { style: 'compressed' })
+
+  fs.ensureDirSync(path.dirname(outputCss))
+  fs.writeFileSync(outputCss, fullResult.css)
+  fs.writeFileSync(outputMinCss, minResult.css)
+}
+
+process.stdout.write('done.\n')
--- scripts/build-js.js
+++ scripts/build-js.js
@@ -1,7 +1,4 @@
 import fs from 'fs-extra'
-import path from 'path'
-import { compile } from 'sass'
-import { globbySync } from 'globby'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 
@@ -11,30 +8,7 @@ const banner = `/* Tabulator v${pkg.version} (c) Oliver Folkerd ${new Date().get
 const minifyTerser = terser({ format: { preamble: banner } })
 const beautifyTerser = terser({ compress: false, mangle: false, format: { beautify: true, preamble: banner } })
 
-const inputFiles = globbySync(['src/scss/themes/*/{,*/}[!_]*.scss'])
-
-console.log('Building Tabulator...')
-
-process.stdout.write('- Cleaning dist folder...')
-fs.rmSync('dist', { recursive: true, force: true })
-process.stdout.write(fs.existsSync('dist') ? ' failed.\n' : ' done.\n')
-
-process.stdout.write('- Compiling SCSS files...')
-for (const inputFile of inputFiles) {
-  const relativeFile = inputFile.replace('src/scss/', '')
-  const outputCss = `dist/css/${relativeFile.replace('.scss', '.css')}`
-  const outputMinCss = `dist/css/${relativeFile.replace('.scss', '.min.css')}`
-
-  const fullResult = compile(inputFile, { style: 'expanded' })
-  const minResult = compile(inputFile, { style: 'compressed' })
-
-  fs.ensureDirSync(path.dirname(outputCss))
-  fs.writeFileSync(outputCss, fullResult.css)
-  fs.writeFileSync(outputMinCss, minResult.css)
-}
-process.stdout.write(' done.\n')
-
-console.log('- Creating JS bundles...')
+process.stdout.write('Creating JS bundles...')
 
 function jsBundle() {
   const browserConfig = {
