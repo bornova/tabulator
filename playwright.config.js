6b9dc2b9c3ce68448ce654c627cf61630af91339
Timur Atalay <timur@atalay.io>
2026-03-08 15:09:11 -0700

    Tests and css

--- playwright.config.js
+++ playwright.config.js
@@ -3,14 +3,12 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './test',
   testMatch: '**/*.spec.js',
-  fullyParallel: true,
   retries: 0,
   reporter: 'list',
-  workers: 1,
   projects: [
     {
       name: 'chromium',
-      use: { browserName: 'chromium', channel: 'chromium', headless: true }
+      use: { browserName: 'chromium', channel: 'chromium' }
     }
   ]
 })
