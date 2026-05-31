b981ba882f4d70682fa7cbcb92626cd33d538946
Timur Atalay <timur@atalay.io>
2026-05-31 16:29:40 -0700

    refactor: remove generateParamsList function from contentTypeFormatters.js

--- src/js/modules/Ajax/defaults/contentTypeFormatters.js
+++ src/js/modules/Ajax/defaults/contentTypeFormatters.js
@@ -1,27 +1,4 @@
-/**
- * Flatten nested values into key/value pairs for request payload generation.
- *
- * @param {*} data Data to flatten.
- * @param {string} [prefix=''] Key prefix.
- * @returns {Array<{key: string|number, value: *}>} Flattened key/value pairs.
- */
-function generateParamsList(data, prefix = '') {
-  let output = []
-
-  if (Array.isArray(data)) {
-    data.forEach((item, i) => {
-      output.push(...generateParamsList(item, prefix ? `${prefix}[${i}]` : i))
-    })
-  } else if (data !== null && typeof data === 'object') {
-    for (const key in data) {
-      output.push(...generateParamsList(data[key], prefix ? `${prefix}[${key}]` : key))
-    }
-  } else {
-    output.push({ key: prefix, value: data })
-  }
-
-  return output
-}
+import { generateParamsList } from './paramsList'
 
 /**
  * Default ajax content-type formatters.
