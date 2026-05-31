205b68505ccd0dc33912f6c55ee5f248d078c315
Timur Atalay <timur@atalay.io>
2026-05-31 16:29:12 -0700

    refactor: replace manual attribute handling with applyElementAttributes function in date editor

--- src/js/modules/Edit/defaults/editors/date.js
+++ src/js/modules/Edit/defaults/editors/date.js
@@ -1,3 +1,5 @@
+import applyElementAttributes from '../applyElementAttributes'
+
 // input element
 /**
  * Date editor using native date input with optional Luxon formatting.
@@ -42,16 +44,7 @@ export default function (cell, onRendered, success, cancel, editorParams) {
     input.setAttribute('min', inputFormat && DT ? convertDate(editorParams.min) : editorParams.min)
   }
 
-  if (editorParams.elementAttributes && typeof editorParams.elementAttributes === 'object') {
-    for (let key in editorParams.elementAttributes) {
-      if (key.charAt(0) === '+') {
-        key = key.slice(1)
-        input.setAttribute(key, input.getAttribute(key) + editorParams.elementAttributes[`+${key}`])
-      } else {
-        input.setAttribute(key, editorParams.elementAttributes[key])
-      }
-    }
-  }
+  applyElementAttributes(input, editorParams.elementAttributes)
 
   cellValue = cellValue !== undefined ? cellValue : ''
 
